63c93684ca40559da841d2042317c613cd8b0f07
Vedant <vedantmohol18@gmail.com>
2024-08-31 15:01:26 +0530

    Connected Database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.mongodb_url)
+.then(()=>{
+    console.log('MongoDb is connected')
+})
+.catch((err)=>{
+    console.log(err)
+});
 
 const app = express();
 
 app.listen(3000,()=>{
     console.log("Server is running on server 3000!")
 });
+//@Krsna18
+//mongodb+srv://vedantmohol18:@Krsna18@blog-vedant1314.uutd2.mongodb.net/?retryWrites=true&w=majority&appName=Blog-Vedant1314
+//mongodb+srv://vedantmohol18:@Krsna18@blog-vedant1314.uutd2.mongodb.net/?retryWrites=true&w=majority&appName=Blog-Vedant1314
