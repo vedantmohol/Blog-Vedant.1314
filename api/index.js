5d8b3c6f9903c911260bb0f2751b918b1cede10e
Vedant <vedantmohol18@gmail.com>
2024-09-01 14:30:46 +0530

    Created user model

--- api/index.js
+++ api/index.js
@@ -17,6 +17,3 @@ const app = express();
 app.listen(3000,()=>{
     console.log("Server is running on server 3000!")
 });
-//@Krsna18
-//mongodb+srv://vedantmohol18:@Krsna18@blog-vedant1314.uutd2.mongodb.net/?retryWrites=true&w=majority&appName=Blog-Vedant1314
-//mongodb+srv://vedantmohol18:@Krsna18@blog-vedant1314.uutd2.mongodb.net/?retryWrites=true&w=majority&appName=Blog-Vedant1314
