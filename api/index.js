cfc2c98d8690666f78a027129a5b82b40c34ac27
Vedant <vedantmohol18@gmail.com>
2024-09-05 16:04:34 +0530

    Created Signup page UI

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use('/api/auth',authRoutes);
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
-    const message = err.meesage || 'Internal Server Error';
+    const message = err.message || 'Internal Server Error';
     res.status(statusCode).json({
         success: false,
         statusCode,
