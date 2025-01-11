7bc5a65a0218e0a1e17fe8a1f76401b6a8a28e39
Vedant <vedantmohol18@gmail.com>
2025-01-11 13:30:01 +0530

    Added comment section to the admin dashboard

--- client/src/Components/DashSidebar.jsx
+++ client/src/Components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from 'flowbite-react';
-import {HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi'; 
+import {HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi'; 
 import React, { useEffect, useState } from 'react'
 import { Link, useLocation} from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
@@ -55,11 +55,18 @@ function DashSidebar() {
                 </Link>
                 )}
                 {currentUser.isAdmin && (
+                  <>
                 <Link to='/dashboard?tab=users'>
                  <Sidebar.Item active={tab==='users'} icon={HiOutlineUserGroup} as='div'>
                   Users
                  </Sidebar.Item>
                 </Link>
+                <Link to='/dashboard?tab=comments'>
+                 <Sidebar.Item active={tab==='comments'} icon={HiAnnotation} as='div'>
+                  Comments
+                 </Sidebar.Item>
+                </Link>
+                  </>
                 )}
                 <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>
                     Sign Out
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import DashSidebar from '../Components/DashSidebar';
 import DashProfile from '../Components/DashProfile';
 import DashPosts from '../Components/DashPosts';
 import DashUsers from '../Components/DashUsers';
+import DashComments from '../Components/DashComments';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -26,6 +27,7 @@ export default function Dashboard() {
       {tab==='profile' && <DashProfile/>}
       {tab==='posts' && <DashPosts/>}
       {tab==='users' && <DashUsers/>}
+      {tab==='comments' && <DashComments/>}
     </div>
   );
 }
