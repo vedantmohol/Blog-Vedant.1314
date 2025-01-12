19f0692a483752e748c8b6e0818d22c439e1110b
Vedant <vedantmohol18@gmail.com>
2025-01-12 15:59:25 +0530

    Completed home page functionality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import CallToAction from '../Components/CallToAction';
+import PostCard from '../Components/PostCard';
 
 export default function Home() {
+  const [posts,setPosts] = useState([]);
+
+  useEffect(()=>{
+    const fetchPosts = async () =>{
+      const res = await fetch('/api/post/getPosts');
+      const data = await res.json();
+      setPosts(data.posts);
+    }
+    fetchPosts();
+  })
   return (
-    <div>Home</div>
+    <div>
+      <div className='flex flex-col gap-6 lg:p-28 px-3 p-28 max-w-6xl mx-auto'>
+        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
+        <p className='text-gray-500 text-xs sm:text-sm'>Explore different types of blogs, articles and tutorials on Engineering related topics.</p>
+      <Link to='/search' className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'>View all post</Link>
+      </div>
+      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+        <CallToAction/>
+      </div>
+      <div className='max-w-6xl mx-auto flex flex-col gap-8 py-7 p-3'>
+        {
+          posts && posts.length > 0 && (
+            <div className='flex flex-col gap-6'>
+              <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+              <div className='flex flex-wrap gap-4'>
+                {posts.map((post)=>{
+                  <PostCard key={post.id} post={post}/>
+                })}
+              </div>
+              <Link to={'/search'} className='text-lg text-teal-500 hover:underline text-center'>View all posts</Link>
+            </div>
+          )
+        }
+      </div>
+    </div>
   )
 }
