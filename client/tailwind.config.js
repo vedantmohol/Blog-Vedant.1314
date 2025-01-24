6d822d91f09880674d8a1bfbcae537e65680748e
Vedant <vedantmohol18@gmail.com>
2025-01-24 14:40:41 +0530

    Fixing errors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,9 @@
-const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
